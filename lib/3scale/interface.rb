0f92befe22b3bca059dcd9307e55728a47571306
adam <adam@3scale.net>
2008-11-05 18:02:04 +0100

    improves documentation

--- lib/3scale/interface.rb
+++ lib/3scale/interface.rb
@@ -2,86 +2,57 @@ require 'cgi'
 require 'hpricot'
 require 'net/http'
 
-module ThreeScale
+module ThreeScale # :nodoc:
   class Error < StandardError; end
 
-  # Base class for errors caused by user.
+  # Base class for exceptions caused by user.
   class UserError < Error; end
 
-  # Error raised when user's contract instance is not active.
+  # Exception raised when contract between user and provider is not active.
+  # Contract can be inactive when it is pending (requires confirmation from
+  # provider), suspended or canceled.
   class ContractNotActive < UserError; end
 
-  # Error raised when usage limits specified by user's contract are exceeded.
+  # Exception raised when usage limits configured for contract are already
+  # exceeded.
   class LimitsExceeded < UserError; end
 
-  # Error raised when user_id is invalid.
+  # Exception raised when +user_key+ is not valid. This can mean that contract
+  # between provider and user does not exists, or the passed +user_key+ does
+  # not correspond to the key associated with this contract.
   class UserKeyInvalid < UserError; end
 
   
-  # Base class for errors caused by provider.
+  # Base class for exceptions caused by provider.
   class ProviderError < Error; end
 
-  # Error raised when some metric names are invalid.
+  # Exception raised when some metric name in provider +usage+ hash does not
+  # correspond to metric configured for the service.
   class MetricInvalid < ProviderError; end
 
-  # Error raised when provider authentication key is invalid.
+  # Exception raised when provider authentication key is not valid. The provider
+  # needs to make sure that the key used is the same as the one that was
+  # generated for him/her when he/she published a service on 3scale.
   class ProviderKeyInvalid < ProviderError; end
 
-  # Error raised when transaction id does not correspond to existing transaction.
+  # Exception raised when transaction corresponding to given +transaction_id+
+  # does not exists. Methods +confirm+ and +cancel+ need valid transaction id
+  # that is obtained by preceding call to +start+.
   class TransactionNotFound < ProviderError; end
 
 
-  # Base class for errors caused by 3scale backend system.
+  # Base class for exceptions caused by 3scale backend system.
   class SystemError < Error; end
+
+  # Other error.
   class UnknownError < SystemError; end
 
 
   
-  # This class provides interface to 3scale backend server.
+  # This class provides interface to 3scale monitoring system.
   #
-  # == Basic usage instructions
-  #
-  # First, create new interface object with 3scale backed hostname and Your
-  # private provider key:
-  #
-  #    interface = ThreeScale::Interface.new("http://3scale.net", "a3b034...")
-  #
-  # Then for each request to Your service:
-  #
-  # 1. Start the transaction with user key and (optionaly) predicted resource
-  # usage (in this example it is: 1 hit and 42000 kilobytes of storage space),
-  #
-  #     transaction = interface.start(user_key, 'hits' => 1, 'storage' => 42000)
-  #
-  # This will return transaction data (if succesful). It is a hash containing
-  # these fields:
-  #
-  #  +:id+: transaction id necessary for confirmation of cancelation of
-  #  transaction (see following steps).
-  #
-  #  +:provider_verification_key+: key You should send back to user so he/she
-  #  can verify the authenticity of the response.
-  #
-  #  +:contract_name+: name of contract the user is signed for. This can be used
-  #  to send different response according to contract type, if that is desired.
-  #
-  # 2. Process the request.
-  #
-  # 3a. If the processing was succesful:
-  # Call +confirm+:
-  #
-  #     interface.confirm(transaction[:id])
-  #
-  # Or call it with actual resource usage, if it differs from predicted one:
-  #
-  #     interface.confirm(transaction[:id], 'hits' => 1, 'storage' => 40500)
-  #
-  # 3b. If there was some error, call +cancel+:
-  #
-  #     interface.cancel(transaction_id)
-  #
-  # 4. Send response back to the user with transaction[:provider_public_key]
-  # embeded.
+  # Objects of this class are stateless and can be shared through multiple
+  # transactions and by multiple clients.
   class Interface
 
     # Hostname of 3scale server.
@@ -91,45 +62,52 @@ module ThreeScale
     # provider and to 3scale.
     attr_accessor :provider_private_key
 
-    # Create a 3scale interface instance.
+    # Create a 3scale interface object.
     #
     # == Arguments
-    # * +host+::                 Hostname of 3scale backend server.
-    # * +provider_private_key+:: Unique key that identifies this provider.
+    # +host+:: Hostname of 3scale backend server.
+    # +provider_private_key+:: Unique key that identifies this provider.
     def initialize(host = nil, provider_private_key = nil)
       @host = host
       @provider_private_key = provider_private_key
     end
 
 
-    # Start a transaction (service request). This can be used also to send
-    # prediction of how much resources will be spend by this request to 3scale
-    # backend server.
+    # Starts a transaction. This can be used also to report estimated resource
+    # usage of the request.
     #
     # == Arguments
-    # * +user_key+:: Key that uniquely identifies an user of the service.
-    # * +usage+::    A hash of that contains metric names and to them
-    #                associated amounts of resources used. For example, if this
-    #                request is going to take 10MB of storage space, then this
-    #                parameter could contain {'storage' => 10}. The values may
-    #                be only approximate or they can be missing altogether. In
-    #                these cases, the real values should be reported using
-    #                method +confirm+.
+    # +user_key+:: Key that uniquely identifies an user of the service.
+    # +usage+::
+    #   A hash of metric names/values pairs that contains predicted resource
+    #   usage of this request.
+    #   
+    #   For example, if this request is going to take 10MB of storage space,
+    #   then this parameter could contain {'storage' => 10}. The values may be
+    #   only approximate or they can be missing altogether. In these cases, the
+    #   real values must be reported using method +confirm+.
     #
     # == Return values
     # A hash containing there keys:
-    # * +id+:: Transaction id. This is required for confirmation/cancellation
-    #          of the transaction later.
-    # * +provider_verification_key+:: This key should be sent back to the user
-    #                                 so he/she can use it to verify the
-    #                                 authenticity of the provider.
-    # * +contract_name+:: This is name of the contract the user is singed for.
-    #                     This information can be used to serve different
-    #                     responses according to contract types,
-    #                     if that is desirable.
+    # <tt>:id</tt>::
+    #   Transaction id. This is required for confirmation/cancellation of the
+    #   transaction later.
+    # <tt>:provider_verification_key</tt>::
+    #   This key should be sent back to the user so he/she can use it to verify
+    #   the authenticity of the provider.
+    # <tt>:contract_name</tt>::
+    #   This is name of the contract the user is singed for. This information
+    #   can be used to serve different responses according to contract types,
+    #   if that is desirable.
     #
     # == Exceptions
     #
+    # ThreeScale::UserKeyInvalid:: +user_key+ is not valid
+    # ThreeScale::ProviderKeyInvalid:: +provider_private_key+ is not valid
+    # ThreeScale::MetricInvalid:: +usage+ contains invalid metrics
+    # ThreeScale::ContractNotActive:: contract is not active
+    # ThreeScale::LimitsExceeded:: usage limits are exceeded
+    # ThreeScale::UnknownError:: some other unexpected error
     #
     def start(user_key, usage = {})
       uri = URI.parse("#{host}/transactions.xml")
@@ -151,9 +129,29 @@ module ThreeScale
       end
     end
 
-    # Confirm previously started transaction.
+    # Confirms a transaction.
     #
-    # TODO: documentation.
+    # == Arguments
+    #
+    # +transaction_id+::
+    #   A transaction id obtained from previous call to +start+.
+    # +usage+::
+    #   A hash of metric names/values pairs containing actual resource usage
+    #   of this request. This parameter is required only if no usage information
+    #   was passed to method +start+ for this transaction, or if it was only
+    #   approximate.
+    #
+    # == Return values
+    #
+    # If there were no exceptions raised, returns true.
+    #
+    # == Exceptions
+    #
+    # ThreeScale::TransactionNotFound:: transactions does not exits
+    # ThreeScale::ProviderKeyInvalid:: +provider_private_key+ is not valid
+    # ThreeScale::MetricInvalid:: +usage+ contains invalid metrics
+    # ThreeScale::UnknownError:: some other unexpected error
+    #
     def confirm(transaction_id, usage = {})
       uri = URI.parse("#{host}/transactions/#{CGI.escape(transaction_id.to_s)}/confirm.xml")
       params = {
@@ -165,9 +163,27 @@ module ThreeScale
       response.is_a?(Net::HTTPSuccess) ? true : handle_error(response.body)
     end
 
-    # Cancel previously started transaction.
+    # Cancels a transaction.
     #
-    # # TODO: documentation.
+    # Use this if request processing failed. Any estimated resource usage
+    # reported by preceding call to +start+ will be deleted. You don't have to
+    # call this if call to +start+ itself failed.
+    #
+    # == Arguments
+    #
+    # +transaction_id+::
+    #   A transaction id obtained from previous call to +start+.
+    #
+    # == Return values
+    #
+    # If there were no exceptions raised, returns true.
+    #
+    # == Exceptions
+    #
+    # ThreeScale::TransactionNotFound:: transactions does not exits
+    # ThreeScale::ProviderKeyInvalid:: +provider_private_key+ is not valid
+    # ThreeScale::UnknownError:: some other unexpected error
+    #
     def cancel(transaction_id)
       uri = URI.parse("#{host}/transactions/#{CGI.escape(transaction_id.to_s)}.xml" +
           "?provider_key=#{CGI.escape(provider_private_key)}")
@@ -179,9 +195,11 @@ module ThreeScale
       response.is_a?(Net::HTTPSuccess) ? true : handle_error(response.body)
     end
 
-    KEY_PREFIX = '3scale-'
+    KEY_PREFIX = '3scale-' # :nodoc:
 
-    # Check if key is for 3scale backend system.
+    # This can be used to quickly distinguish between keys used with 3scale
+    # system and any other keys the provider might use. Returns true if the key
+    # is for 3scale system.
     def system_key?(key)
       # Key should start with prefix
       key.index(KEY_PREFIX) == 0
@@ -207,8 +225,7 @@ module ThreeScale
       'user.inactive_contract' => ContractNotActive,
       'provider.invalid_key' => ProviderKeyInvalid,
       'provider.invalid_metric' => MetricInvalid,
-      'provider.invalid_transaction_id' => TransactionNotFound
-    }
+      'provider.invalid_transaction_id' => TransactionNotFound} # :nodoc:
 
     def handle_error(response)
       element = Hpricot::XML(response).at('error')
